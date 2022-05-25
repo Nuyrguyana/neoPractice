b4298e4c4e422ee11b91e450c663ce7748a03f8f
Nuyrguyana <enee.kors@mail.ru>
2022-05-25 22:49:44 +0300

    Initial commit

---
+++ claims-client/src/App.jsx
@@ -0,0 +1,13 @@
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import {Main} from "./container/main";
+
+const App = () => {
+    return (
+        <div>
+            <Main/>
+        </div>
+    );
+}
+export default App;
