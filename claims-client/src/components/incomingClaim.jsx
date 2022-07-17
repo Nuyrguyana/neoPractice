9d965897acab3020a867213fcced9f495351f30b
Nuyrguyana <enee.kors@mail.ru>
2022-07-17 23:54:58 +0300

    update claim by PUT-request

--- claims-client/src/components/incomingClaim.jsx
+++ claims-client/src/components/incomingClaim.jsx
@@ -6,6 +6,7 @@ import { getToken } from '../api/jwtLocalStorage';
 
 export const IncomingClaim = () => {
     const [claim, setClaim] = useState({})
+    const [statuses, setStatuses] = useState([])
     const {claimId} = useParams()
     const history = useHistory()
 
@@ -13,52 +14,98 @@ export const IncomingClaim = () => {
         history.push('/');
     };
 
-
     useEffect(() => {
+        // to prevent unsubscribe error #1
+        let isSubscribed = true;
+
         axios.get(`http://localhost:3001/claim/${claimId}`, {
             headers: {
                 Authorization: "Bearer " + getToken()
             }
-        }).then((resp) => {
-            console.log("claim by id", resp.data);
-        })
+        }).then((resp) => isSubscribed ? setClaim(resp.data) : null
+        ).catch((error) => console.error(error))
+
+        axios.get('http://localhost:3001/status', {
+            headers: {
+                Authorization: "Bearer " + getToken()
+            }
+        }).then((resp) => isSubscribed ? setStatuses(resp.data) : null
+        ).catch((error) => console.error(error))
+
+        // to prevent unsubscribe error #2
+        return () => (isSubscribed = false)
     }, [])
 
-    const handleClick = () => {
+    const handleStatusChange = (newStatus) => {
+        console.log("newStatus", newStatus);
+        const status = statuses.find((el) => el.name === newStatus)
+        updateClaim({
+            title: claim.title,
+            description: claim.description,
+            type: claim.type.slug,
+            status: status.slug,
+        })
+    }
 
-        history.push('/')
+    const handleTextFieldChange = ({target}) => {
+        setClaim((prevState) => ({
+            ...prevState,
+            [target.name]: target.value
+        }))
+    }
+
+    const updateClaim = (updatedClaim) => {
+        console.log('updatedClaim', updatedClaim)
+        axios.put(`http://localhost:3001/claim/${claimId}`, updatedClaim, {
+            headers: {
+                Authorization: "Bearer " + getToken()
+            }
+        }).then((resp) => {
+            console.log(resp)
+            history.push('/')
+        }).catch((error) => {
+            console.error(error)
+        })
     }
 
     return (
-        <div className='container-incoming-claim'>
+        <form className='container-incoming-claim'>
             <h2 className='main-title'>Incoming claim</h2>
             <label className='label-claim'>TITLE</label>
             <input className='claim-editing'
-                   placeholder={claim.title}/>
+                   placeholder={claim.title}
+                   name='title'
+                   onChange={handleTextFieldChange}
+            />
             <div className='claim-type'>
                 <label className='label-claim'>TYPE</label>
-                <SelectField/>
+                <SelectField claim={claim} setClaim={setClaim}/>
             </div>
             <label className='label-claim'>DESCRIPTION</label>
-            <input className='claim-editing'/>
+            <input className='claim-editing'
+                   placeholder={claim.description}
+                   name='description'
+                   onChange={handleTextFieldChange}
+            />
             <div>
                 <button className='btn-ic btn-cancel'
-                        onClick={() => {
-                            handleAllClaims()
-                        }}>Cancel
+                        onClick={() => handleAllClaims()}>
+                    Cancel
                 </button>
                 <button className='btn-ic btn-done'
-                        onClick={() => {
-                            handleClick()
-                        }}>Done
+                        onClick={() => handleStatusChange('Done')}>
+                    Done
                 </button>
                 <button className='btn-ic btn-decline'
-                        onClick={() => {
-                            handleAllClaims()
-                        }}>Decline
+                        onClick={() => handleStatusChange('Declined')}>
+                    Decline
+                </button>
+                <button className='btn-ic btn-inprogress'
+                        onClick={() => handleStatusChange('In progress')}>
+                    In progress
                 </button>
             </div>
-        </div>
+        </form>
     );
 };
 
--- claims-client/src/components/selectField.jsx
+++ claims-client/src/components/selectField.jsx
@@ -1,22 +1,56 @@
 import React, { useEffect, useState } from 'react';
-import claimsAPI from '../api/fake.api'
-import { useParams } from "react-router-dom";
+import axios from 'axios';
+import { getToken } from '../api/jwtLocalStorage';
 
 
-export const SelectField = () => {
-    const [claim, setClaim] = useState({})
-    const {claimId} = useParams()
+export const SelectField = ({claim, setClaim}) => {
+    const [types, setTypes] = useState([])
 
     useEffect(() => {
-        claimsAPI
-            .getById(claimId)
-            .then(data => setClaim(data))
-    }, [])
+        // to prevent unsubscribe error #1
+        let isSubscribed = true;
+
+        axios.get('http://localhost:3001/types', {
+            headers: {
+                Authorization: "Bearer " + getToken()
+            }
+        }).then((resp) => {
+
+            const typeArray = resp.data.filter((type) => {
+                return type.name !== claim.type?.name
+            })
+            if (isSubscribed) {
+                setTypes(typeArray)
+            }
+        })
+
+        // to prevent unsubscribe error #2
+        return () => (isSubscribed = false)
+    }, [claim])
+
+    const handleChange = ({target}) => {
+        setClaim((prevState) => ({
+            ...prevState,
+            [target.name]: types.filter((el) => {
+                return el.name === target.value
+            })[0]
+        }));
+    }
+
+    // console.log("selField claim", claim)
     return (
-        <select className='type-select'>
-            <option> {claim.type}</option>
-            <option> hardware</option>
-            <option> tr</option>
+        <select
+            className='type-select'
+            onChange={handleChange}
+            name='type'>
+            <option key={claim.type?.name}>
+                {claim.type?.name}
+            </option>
+            {types.map((type) => {
+                return <option key={type.name}>
+                    {type.name}
+                </option>
+            })}
         </select>
     );
 };
