e0e28816ae9325eb3ff8e49985c1acdbb7797f1a
Nuyrguyana <enee.kors@mail.ru>
2022-07-19 17:25:06 +0300

    add creating new claim feature

--- claims-client/src/components/creatingNewClaim.jsx
+++ claims-client/src/components/creatingNewClaim.jsx
@@ -1,38 +1,104 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from "react-router-dom";
+import axios from 'axios';
+import { getToken } from '../api/jwtLocalStorage';
 
 export const CreatingNewClaim = () => {
     const history = useHistory()
+    const [claim, setClaim] = useState({})
+    const [types, setTypes] = useState([])
+
+    useEffect(() => {
+        let isSubscribed = true;
+
+        axios.get('http://localhost:3001/types', {
+            headers: {
+                Authorization: "Bearer " + getToken()
+            }
+        }).then((resp) => {
+
+            const typeArray = resp.data.filter((type) => {
+                return type.name !== claim.type?.name
+            })
+            if (isSubscribed) {
+                setTypes(typeArray)
+            }
+        })
+
+        return () => (isSubscribed = false)
+    }, [])
 
     const handleAllClaims = () => {
         history.push('/');
     };
+
+    const handleTextFieldChange = ({target}) => {
+        setClaim((prevState) => ({
+            ...prevState,
+            [target.name]: target.value
+        }))
+    }
+
+    const handleChange = ({target}) => {
+        setClaim((prevState) => ({
+            ...prevState,
+            [target.name]: types.find((el) => el.name === target.value)
+        }));
+    }
+
+    const createClaim = () => {
+        axios.post('http://localhost:3001/claim', {
+            "title": claim.title,
+            "description": claim.description,
+            "type": claim.type.slug
+        }, {
+            headers: {
+                Authorization: "Bearer " + getToken()
+            }
+        }).then((resp) => {
+            console.log(resp)
+            history.push('/')
+        }).catch((error) => {
+            console.error(error)
+        })
+    }
 
     return (
         <div className='container-incoming-claim'>
             <h2 className='main-title'>Creating new claim</h2>
             <label className='label-claim'>TITLE</label>
             <input className='claim-editing'
-                   placeholder='Type claim title'/>
+                   name='title'
+                   placeholder='Type claim title'
+                   onChange={handleTextFieldChange}
+            />
             <div className='claim-type'>
                 <label className='label-claim'>TYPE</label>
-                <select className='type-select'>
-                    <option> software</option>
-                    <option> hardware</option>
-                    <option> tr</option>
-
+                <select className='type-select'
+                        name='type'
+                        onChange={handleChange}
+                >
+                    {types.map((type) => {
+                        return <option key={type.name}>{type.name}</option>
+                    })}
                 </select>
             </div>
             <label className='label-claim'>DESCRIPTION</label>
             <input className='claim-editing'
-                   placeholder='Type claim description'/>
+                   name='description'
+                   placeholder='Type claim description'
+                   onChange={handleTextFieldChange}
+            />
             <div>
                 <button className='btn-ic btn-cancel'
                         onClick={() => {
                             handleAllClaims()
                         }}>Cancel
                 </button>
-                <button className='btn-ic btn-done'>Create</button>
+                <button className='btn-ic btn-done'
+                        onClick={createClaim}
+                >Create
+                </button>
             </div>
         </div>
     );
